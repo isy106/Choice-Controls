f9543835d59eca42e2b58843a2a06bf481354f1d
isy106 <63172916+isy106@users.noreply.github.com>
2020-09-10 13:51:21 -0400

    Add project files.

---
+++ FrmChoices.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmChoices
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cboSaleItems = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(31, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Combo box - Sale items"
+        '
+        'cboSaleItems
+        '
+        Me.cboSaleItems.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboSaleItems.FormattingEnabled = True
+        Me.cboSaleItems.Location = New System.Drawing.Point(34, 53)
+        Me.cboSaleItems.Name = "cboSaleItems"
+        Me.cboSaleItems.Size = New System.Drawing.Size(117, 21)
+        Me.cboSaleItems.TabIndex = 1
+        '
+        'FrmChoices
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cboSaleItems)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FrmChoices"
+        Me.Text = "Controls that provide choices"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cboSaleItems As ComboBox
+End Class
---
+++ FrmChoices.vb
@@ -0,0 +1,16 @@
+﻿Public Class FrmChoices
+
+    Private Sub LoadComboBoxChoices()
+        cboSaleItems.Items.Add("Long Sleeve Shirt")
+        cboSaleItems.Items.Add("Short Sleeve Shirt")
+        cboSaleItems.Items.Add("Lightweight Jacket")
+    End Sub
+
+    Private Sub FrmChoices_Load(sender As Object, e As EventArgs) Handles Me.Load
+        LoadComboBoxChoices()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
